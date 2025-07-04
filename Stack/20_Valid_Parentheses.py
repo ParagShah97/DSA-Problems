8480ba832864589480424befa0ba312a566151dd
ParagShah97 <parag.shah97@gmail.com>
2025-07-04 14:04:57 -0400

    Valid Parentheses

--- Stack/20_Valid_Parentheses.py
+++ Stack/20_Valid_Parentheses.py
@@ -1,47 +1,32 @@
 # https://leetcode.com/problems/valid-parentheses
-# Time Complexity: O(N)
-# Space Complexity: O(N)
+# Time complexity: O(N)
+# Space complexity: O(N)
 class Solution:
     def isValid(self, s: str) -> bool:
         """
         Intuition:
-        The question can be done on other way with relaively less conditions, though
-        I want to cover all the cases.
-
-        So, if the length of s is odd then return False.
-        Iterate over s from index 1 to n, as 0th element already in stack.
-
-        If the current element in compare dict then we can directly append it to 
-        the stack (i.e-> (, {, [ ).
-        If stack is not empty and stack top element in compare then
-        if current element == value agaist the compare's stack top element, then
-        pop the element from the stack, continue to next element
-        else, return False (mean stack is not empty and top element not a match
-        for the current element i.e. "(}" )
-
-        At last return not stack (if stack empty return True else False)
+        First we can access that the length of the string is odd then definately
+        there is no valid parentheses: return False.
+        For other scenarios we will take stack and a mapping dic for parantheses.
+        (see code for dic)
+        We iterate over the string s. First we check if the val of b is present in
+        dic "({[" are present then append it to the stack.
+        If we get ")]}" as b's value then we first check if the stack is non empty,
+        if non-empty then we check if the stack top element's mapped value == b,
+        then just pop the element from the stack. Else we return False, as we get the
+        closing parantheses which is not closing the current one.
+        At last we return True if stack is empty else False. 
         """
-
-        if len(s) % 2 == 1:
+        if len(s)%2 == 1:
             return False
-
-        stack=[s[0]]
-        compare = {"(":")", "[":"]", "{":"}"}
-
-        for i in range(1, len(s)):
-            # print("ss ", stack, s[i])
-            if s[i] in compare:
-                stack.append(s[i])
-                continue
-            if len(stack) and stack[-1] in compare:
-                if compare[stack[-1]] == s[i]:
-                    stack.pop()
-                    continue
-                return False
+        mappingDic = {"{":"}", "(":")", "[":"]"}
+        stack = []
+        for b in s:
+            if b in mappingDic:
+                stack.append(b)
+            elif stack and mappingDic[stack[-1]] == b:
+                stack.pop()
             else:
                 return False
-                
         
-        return not stack 
-
-        
+        return not stack
